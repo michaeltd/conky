da23ccc4b304a774695c0459b5f3674bc11d128a
michaeltd <tsouchlarakis@gmail.com>
2020-05-06 08:43:20 +0300

    All your codebase are belong to us.

--- README.md
+++
@@ -1,2 +0,0 @@
-# conky
-conkyrc for all
---
+++ bootstrap-theme.bash
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+main()
+{
+
+    isValidLink(){:}
+
+    isValidTheme(){:}
+
+    themeHasFonts(){:}
+
+    themeHasRc(){:}
+
+    themeHasConkyrc(){:}
+    
+}
+
+# Run on execute not source!
+[[ "${BASH_SOURCE[0]}" == "${0}"]] && main "${@}"
